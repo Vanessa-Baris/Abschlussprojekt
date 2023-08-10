f72136d2017e0df616e562638b2cd8e0cc1c9cd6
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-08-10 12:00:54 +0200

    Zwischenschritt alles überarbeitet

--- src/main/kotlin/Gegner.kt
+++ src/main/kotlin/Gegner.kt
@@ -1,2 +1,18 @@
-class Gegner {
+open class Gegner {
+    val actions = mutableListOf<Action>()
+
+    open fun addAction(action: Action) {
+        actions.add(action)
+    }
+
+    open fun useAction2(target: Held) {
+        if (actions.isNotEmpty()) {
+            val action = actions[0]
+            action.execute(target)
+            actions.removeAt(0)
+        } else {
+            println("$name hat keine Attacken mehr.")
+        }
+    }
+}
 }
