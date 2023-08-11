2041cb3e55b9180d4ea67c252661a1ef3b80346f
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-08-11 10:22:58 +0200

    Held Class Beutel integriert

--- src/main/kotlin/Held.kt
+++ src/main/kotlin/Held.kt
@@ -1,20 +1,32 @@
-open class Held( val name: String,  var hp: Int)  {
+open class Held( val name: String,  var hp: Int) {
 
     var hasActedThisRound: Boolean = false
+    var hasUsedBeutelThisRound: Boolean = false
+
 
     fun resetRound(): Boolean {
         hasActedThisRound = false
-        return true
-    }
+        hasUsedBeutelThisRound = false
+
+
 
     fun canActThisRound(): Boolean {
         return hasActedThisRound
+    }
+
+    fun markBeutelAsUsedThisRound() {
+        hasUsedBeutelThisRound = true
     }
 
     fun markAsActedThisRound(): Boolean {
         hasActedThisRound = true
         return true
     }
+
+    fun canUseBeutelThisRound(): Boolean {
+        return hasUsedBeutelThisRound && hasActedThisRound
+    }
+
 
     fun performAction(target: Gegner) {
         if (canActThisRound()) {
@@ -25,14 +37,23 @@ open class Held( val name: String,  var hp: Int)  {
         }
     }
 
+    fun useBeutel(beutel: Beutel) {
+        if (canUseBeutelThisRound()) {
+            println("$name nutzt den Beutel.")
+            beutel.markAsUsedThisRound()
+            markBeutelAsUsedThisRound()
+        } else {
+            println("$name kann den Beutel in dieser Runde nicht mehr nutzen.")
+        }
+    }
 
 }
 
     //Von Chat GPT Anregung geholt und auf meine Bedürfnisse umgewandelt, da ich nicht wusste, wie ich blocken soll:
      val actions: MutableList<(Gegner) -> Unit> = mutableListOf()
-//mutableListOf() habe ich aber selbst angelegt.
+    //mutableListOf() habe ich aber selbst angelegt.
 
-//Ab hier dann wieder Chat GPT:
+    //Ab hier dann wieder Chat GPT:
     var isBlocked: Boolean = false
 
     fun addAction(action: (Gegner) -> Unit) {
@@ -41,14 +62,15 @@ open class Held( val name: String,  var hp: Int)  {
 
     fun useAction(target: Gegner) {
         if (actions.isNotEmpty()) {
-        val action = actions[0]
-        if (isBlocked) {
-            println("Echo ist geschützt und blockt die nächste Aktion.")
-            isBlocked = false
-        } else {
-            action(target)
+            val action = actions[0]
+            if (isBlocked) {
+                println("Echo ist geschützt und blockt die nächste Aktion.")
+                isBlocked = false
+            } else {
+                action(target)
+            }
+            actions.removeAt(0)
         }
-        actions.removeAt(0)
     }
 }
 
