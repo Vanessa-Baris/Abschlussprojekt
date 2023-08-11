d3a28b245fb70864c027af797dde15b3b341b9c0
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-08-11 10:05:59 +0200

    private raus, lohnt nicht

--- src/main/kotlin/Held.kt
+++ src/main/kotlin/Held.kt
@@ -24,10 +24,12 @@ open class Held( val name: String,  var hp: Int)  {
             println("$name hat bereits in dieser Runde gekämpft.")
         }
     }
+
+
 }
 
     //Von Chat GPT Anregung geholt und auf meine Bedürfnisse umgewandelt, da ich nicht wusste, wie ich blocken soll:
-    private val actions: MutableList<(Gegner) -> Unit> = mutableListOf()
+     val actions: MutableList<(Gegner) -> Unit> = mutableListOf()
 //mutableListOf() habe ich aber selbst angelegt.
 
 //Ab hier dann wieder Chat GPT:
