ca7423716294ead1e2d0fa0cc6b0cea7ee3e9e63
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-08-11 09:59:53 +0200

    Beutel kt,  als nächstes schauen wie ich die Vitamine  mache

--- src/main/kotlin/Beutel.kt
+++ src/main/kotlin/Beutel.kt
@@ -1,2 +1,24 @@
-class Beutel {
+class Beutel() {
+    var hasUsedThisRound: Boolean = false
+
+    fun resetRound() {
+        hasUsedThisRound = false
+}
+
+    fun canUseThisRound(): Boolean {
+    return hasUsedThisRound
+}
+
+    fun markAsUsedThisRound() {
+        hasUsedThisRound = true
+}
+
+    fun useBag() {
+        if (canUseThisRound()) {
+            println("Der Beutel wurde genutzt.")
+            markAsUsedThisRound()
+     } else {
+            println("Der Beutel wurde bereits in dieser Runde genutzt.")
+        }
+    }
 }
