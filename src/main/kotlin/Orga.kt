ae33c44c0d9826c86bd786801b789867e5921923
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-08-14 09:47:31 +0200

    Verbessert

--- src/main/kotlin/Orga.kt
+++ src/main/kotlin/Orga.kt
@@ -3,7 +3,8 @@
 //1.0 Held 1: Name: Oreade , Leben(Hp): 50 , Vier Aktionen einmalig pro Runde: Blütenzauber mit 70 Schaden | Hellsicht (maybe das sie die nächste Attacke des Gegners blockt) mit 60 Schaden | Dunkle Dornen mit 30 Schaden | Baumstamm Weitwurf mit 40 Schaden
 //1.1 Held 2: Name: Vampir , Leben(Hp): 70 , Vier Aktionen einmalig pro Runde: Biss mit 44 Schaden | Tarnumhang mit 5 Schaden | Manipulation mit 30 Schaden | Kratzen mit 20 Schaden
 //1.2 Held 3: Name: Zombie , Leben(Hp): 60 , Vier Aktionen alle nur einmalig pro Runde ausführbar: Schubsen mit 33 Schaden | Jagen mit 40 Schaden | Knabbern mit 20 Schaden | Wegtorkeln flieht und setzt eine Runde aus
-//Beutel der Heldengruppe mit Sofort Effekt: einmalig pro Runde, wer ihn nutzt kann in dieser Runde keine weitere Aktion mehr ausführen , 1x Starker Heiltrank der 30 Leben gibt | 1x bitteres Kraut das dauerhaft 10 Leben in der Runde gibt | 1x Umkehrfluch der 50 Leben gibt | 1x Unsterblichkeitstrank der 70 Leben gibt
+//Beutel der Heldengruppe mit Sofort Effekt: einmalig pro Runde, wer ihn nutzt kann in dieser Runde keine weitere Aktion mehr ausführen
+
 
 //Endgegner: Name: Magier , Leben(Hp): 200 , Sechs Aktionen einmalig pro Runde: Verätzung zieht jedem Spieler gleichzeitig 20 Leben ab | Fluch der nur einen Helden trifft und von diesem Helden solange die HP um 10% pro Runde verringeren, bis die HP des entsprechenden Helden weniger als 20% seiner gesamten HP beträgt | Beschwörung beschwört den Helfer den man dann auch besiegen muss | Feuerbälle mit 30 Schaden auf einen Helden | Orkan mit 50 Schaden auf einen Helden | Heuler mit 10 Schaden auf einen Helden
 //Helfer: Name: Golem , Leben(Hp): 30 , Vier Aktionen nur einmal pro Runde ausführbar: Schlagen mit 10 Schaden | Schlammatem mit 20 Schaden | Kicken mit 40 Schaden | Fuchteln mit 5 Schaden
