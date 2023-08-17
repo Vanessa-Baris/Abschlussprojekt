3059b555492e0ed19073ee2702ba4caab34a95ec
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-08-17 20:49:54 +0200

    Letzter Feinschliff

--- src/main/kotlin/Gegner.kt
+++ src/main/kotlin/Gegner.kt
@@ -2,10 +2,12 @@ open class Gegner(var name: String, var hp: Int) {
 
     val actions: MutableList<(Held) -> Unit> = mutableListOf()
 
+    // Fügt eine Aktion zur Liste hinzu
     fun addAction(action: (Held) -> Unit) {
         actions.add(action)
     }
 
+    //guckt ob tot ist der Gegner
     open fun isDead(): Boolean {
         return hp <= 0
     }
